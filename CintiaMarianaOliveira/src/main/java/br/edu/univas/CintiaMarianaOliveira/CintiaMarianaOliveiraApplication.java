348d02d6f47c8691c1affd2b91e4952a11bd25bd
CintiaMariana <81639655+CintiaMariana@users.noreply.github.com>
2024-04-16 19:17:03 -0300

    Add files via upload

---
+++ CintiaMarianaOliveira/src/main/java/br/edu/univas/CintiaMarianaOliveira/CintiaMarianaOliveiraApplication.java
@@ -0,0 +1,13 @@
+package br.edu.univas.CintiaMarianaOliveira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CintiaMarianaOliveiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CintiaMarianaOliveiraApplication.class, args);
+	}
+
+}
